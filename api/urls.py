0600185ae4db79c7a93e86095b1acec9163417e4
Atid khodaie <amirmahdijafarian@gmail.com>
2021-10-31 18:00:14 +0330

    user serializer and some other changes

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ArticlesAPI
+from .views import ArticlesAPI, RetriveEditDeleteArticleApi, UserApi
 
 app_name = 'api'
 
 
 urlpatterns = [
-    path('',ArticlesAPI.as_view(), name='article-api')
+    path('',ArticlesAPI.as_view(), name='article-api'),
+    path('user/<int:pk>', UserApi.as_view(), name='edit-user'),
+    path('<int:pk>', RetriveEditDeleteArticleApi.as_view(), name='edit-article'), #u can use slug instead of pk <slug:slug>
+    # path('<int:pk>/retrive', RetrieveAPIView.as_view(), name='retrive-article'),
+    # path('<int:pk>/update', UpdateArticleApi.as_view(), name='update-article'),
+    # path('<int:pk>/delete', DestroyArticleApi.as_view(), name='destroy-article'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIView
-from .serializers import ArticleSerializer
+from django.shortcuts import render, get_object_or_404
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+    RetrieveAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+)
+from .serializers import (
+    ArticleSerializer,
+    UserSerializer,
+)
 from blog.models import Article
-
+from django.contrib.auth.models import User
 
 class ArticlesAPI(ListAPIView):
     def get_queryset(self):
         return Article.objects.all()
     serializer_class = ArticleSerializer
-# Create your views here.
+#
+# class EditAndDeleteArticle(RetrieveUpdateDestroyAPIView):
+#     def get_object(self):
+#         return get_object_or_404(Article.objects.all(),pk=self.kwargs.get('pk'))
+#     serializer_class = ArticleSerializer
+
+class RetriveEditDeleteArticleApi(RetrieveUpdateDestroyAPIView):
+    def get_object(self):
+        return get_object_or_404(Article.objects.all(),pk=self.kwargs.get('pk'))
+    serializer_class = ArticleSerializer
+
+class UserApi(RetrieveUpdateDestroyAPIView):
+    def get_object(self):
+        return get_object_or_404(User.objects.all(),pk=self.kwargs.get('pk'))
+    serializer_class = UserSerializer
